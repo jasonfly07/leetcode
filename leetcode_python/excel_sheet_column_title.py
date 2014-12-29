d19671f084f983f09903c8087a925296104f9043
jasonfly07 <shengchieh1989@gmail.com>
2014-12-30 02:23:01 +0800

    add excel

---
+++ leetcode_python/excel_sheet_column_title.py
@@ -0,0 +1,46 @@
+import numpy as np
+import math
+
+class Solution:
+    # This is called "bijective base-26 conversion" (1-26 instead of 0-25)
+    # Due to the lack of 0, we deduct 1 from num everytime we shift to the next digit
+    # it's a bit unintuitive to understand.
+    def convertToTitle(self, num):
+        output = ""
+
+        while num>0:
+            num -= 1
+            q = num%26
+            r = num/26
+            currChar = chr( ord('A')+q )
+            output = currChar+output
+            num = r
+
+        return output
+
+    # This is regular base-26 conversion (for comparison)
+    def base26(self, num):
+        allDigits = []
+
+        while num >= 26:
+            q = num/26
+            r = num%26
+            allDigits.append(r)
+            num = q
+        allDigits.append(num)
+
+        return allDigits
+            
+
+
+
+def main():
+    num = 702
+
+    s1 = Solution()
+    print s1.convertToTitle(num)
+
+    
+
+if __name__ == "__main__":
+    main()
